c37a199aec2c0d8e2469d0fb7776128c63103876
SonHongJoo <shj0917@naver.com>
2020-05-12 13:31:42 +0900

    build 경로 변경

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/vue-ts-test/' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/vue-ts-blog/' : '/',
     outputDir: 'docs'
 }
